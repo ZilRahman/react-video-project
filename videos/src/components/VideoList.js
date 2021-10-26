cdeae19be8409cbae346fe21b41efd2d2b1b0a3a
ZilRahman <77643517+ZilRahman@users.noreply.github.com>
2021-10-26 10:12:46 -0600

    individual video objects are rendered inside VideoList as well as their thumbnails

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 // method component that is passed videos from App.js and returns a list of Videos
-const VideoList = props => {
-    return <div>{props.videos.length}</div> 
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem video={video} /> })
+
+    return <div>{renderedList}</div> 
 }
 
 export default VideoList;
